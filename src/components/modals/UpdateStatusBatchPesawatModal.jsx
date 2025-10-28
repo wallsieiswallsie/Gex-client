eefdc3eeb1733fe78ea53c544e456c19f884a737
wallsieiswallsie <ralazinawla@gmail.com>
2025-10-28 08:33:04 +0700

    debug

---
+++ src/components/modals/UpdateStatusBatchPesawatModal.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { addPackageStatus, fetchBatchPesawatDetailApi } from "../../utils/api";
+
+export default function UpdateStatusBatchModal({ batch, onClose, onUpdated }) {
+  const [status, setStatus] = useState(batch.status || "");
+
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains("modal-overlay")) {
+      onClose();
+    }
+  };
+
+const handleSubmit3 = async (e) => {
+  e.preventDefault();
+  try {
+    const resJson = await fetchBatchPesawatDetailApi(batch.id);
+    const batchData = resJson.data;
+
+    console.log("packages:", batchData.packages);
+
+    if (!batchData || !batchData.packages || batchData.packages.length === 0) {
+      alert("Tidak ada paket dalam batch ini");
+      return;
+    }
+
+    await addPackageStatus(null, 3, batch.id);
+
+    alert("Semua paket dalam batch berhasil diupdate ke status 3!");
+    onUpdated();
+    onClose();
+  } catch (err) {
+    console.error(err);
+    alert("Terjadi kesalahan saat update status batch");
+  }
+};
+
+const handleSubmit4 = async (e) => {
+  e.preventDefault();
+  try {
+    const resJson = await fetchBatchPesawatDetailApi(batch.id);
+    const batchData = resJson.data;
+
+    console.log("packages:", batchData.packages);
+
+    if (!batchData || !batchData.packages || batchData.packages.length === 0) {
+      alert("Tidak ada paket dalam batch ini");
+      return;
+    }
+
+    await addPackageStatus(null, 4, batch.id);
+
+    alert("Semua paket dalam batch berhasil diupdate ke status 4!");
+    onUpdated();
+    onClose();
+  } catch (err) {
+    console.error(err);
+    alert("Terjadi kesalahan saat update status batch");
+  }
+};
+
+  return (
+    <div className="modal-overlay" onClick={handleOverlayClick}>
+      <div className="modal-container">
+        <h2 className="modal-title">Update Status Batch</h2>
+        <form onSubmit={handleSubmit3}>
+            <button
+              type="submit"
+              className="update_status_batch_modal-button"
+            >
+              Sudah Berangkat
+            </button>
+        </form>
+        <form onSubmit={handleSubmit4}>
+            <button
+              type="submit"
+              className="update_status_batch_modal-button"
+            >
+              Sudah Sampai
+            </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/pages/batches/DisplayBatchesPesawat.jsx
+++ src/pages/batches/DisplayBatchesPesawat.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import { fetchBatchesPesawatApi, createBatchesPesawatApi } from "../../utils/api";
-import UpdateStatusBatchModal from "../../components/modals/UpdateStatusBatchModal";
+import UpdateStatusBatchPesawatModal from "../../components/modals/UpdateStatusBatcPesawathModal";
 
 export default function DisplayBatchesPesawat() {
   const { user } = useAuth();
@@ -161,7 +161,7 @@ export default function DisplayBatchesPesawat() {
 
       {/* Modal Update */}
       {selectedBatch && (
-        <UpdateStatusBatchModal
+        <UpdateStatusBatchPesawatModal
           batch={selectedBatch}
           onClose={() => setSelectedBatch(null)}
           onUpdated={fetchData}
